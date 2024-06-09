6b0ee1b8eff7591b45c4a66d8b1e75d921f96257
Pau <pauguvi@gmail.com>
2024-06-09 17:32:27 +0200

    implement auth

--- src/database.js
+++ src/database.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 export const databaseCall = (method = "GET", endpoint, gameId, data) => {
-  return axios(`${process.env.BACKEND_URL}/games/${gameId}${endpoint}`, {
+  return axios(`${process.env.BACKEND_URL}/games/${gameId}${endpoint}?api_key=${process.env.BACKEND_API_KEY}`, {
     method: method,
     headers: {
       "Content-Type": "application/json",
